75822821c12ff7754e3747bec27c7e02a2be0cdd
therealJimoh <jimoh.abdullah69@gmail.com>
2022-08-09 15:44:49 +0100

    build and deploy a machine learning model

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
+# This commands will upload an image to Docker Hub
 
-# Step 1:
 # This is your Docker ID/path
 dockerpath=therealjimoh/sklearn:v1.0.0
 
-# Step 2
 # Run the Docker Hub container with kubernetes
 kubectl run sklearn --image=therealjimoh/sklearn:v1.0.0 --port=80 --labels app=sklearn
 
 
-# Step 3:
 # List kubernetes pods
 kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
 kubectl port-forward sklearn 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
+# This commands uploads an image to Docker Hub
 
-# Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
 # Create dockerpath
 dockerpath=therealjimoh/sklearn:v1.0.0
 
-# Step 2:  
 # Authenticate & tag
 docker login
 docker tag sklearn $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
-# Step 3:
 # Push image to a docker repository
 docker push $dockerpath
